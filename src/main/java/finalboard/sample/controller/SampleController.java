01d91137e7235cfc8c5bcdfd6fa55ad0e2565117
KH_I\user1 <cayori@gmail.com>
2017-12-21 15:11:30 +0900

    1324줄까지 코딩. boardUpdate.jsp 만 하면 됨 ojdbc7.jar mvn 설치 해줄것

---
+++ src/main/java/finalboard/sample/controller/SampleController.java
@@ -0,0 +1,19 @@
+package finalboard.sample.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import finalboard.common.common.CommandMap;
+import finalboard.sample.service.SampleService;
+
+@Controller
+public class SampleController {
+
+}
